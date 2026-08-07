c33ada813e76bf6cc9a5c7b819a9ed5a9c50a306
LunaAstryx <luna.zeul@gmail.com>
2026-08-07 14:36:36 +0200

    flower fix👀

--- src/main/java/lunaastryx/random_bs/datagen/ModModelProvider.java
+++ src/main/java/lunaastryx/random_bs/datagen/ModModelProvider.java
@@ -15,8 +15,8 @@ public class ModModelProvider extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockModelGenerators blockModelGenerators) {
-        blockModelGenerators.createPlant(ModBlocks.PURPLE_TULIP, ModBlocks.POTTED_PURPLE_TULIP, BlockModelGenerators.PlantType.NOT_TINTED);
-        blockModelGenerators.createPlant(ModBlocks.YELLOW_TULIP, ModBlocks.POTTED_YELLOW_TULIP, BlockModelGenerators.PlantType.NOT_TINTED);
+        blockModelGenerators.createPlantWithDefaultItem(ModBlocks.PURPLE_TULIP, ModBlocks.POTTED_PURPLE_TULIP, BlockModelGenerators.PlantType.NOT_TINTED);
+        blockModelGenerators.createPlantWithDefaultItem(ModBlocks.YELLOW_TULIP, ModBlocks.POTTED_YELLOW_TULIP, BlockModelGenerators.PlantType.NOT_TINTED);
 
     }
 
